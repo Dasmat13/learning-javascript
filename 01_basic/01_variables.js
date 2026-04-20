6b8c0a9e3d2af92f5734188a847a6c9ecdf9410c
Dasmat13 <mey37056@gmail.com>
2026-04-21 01:52:26 +0530

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 34571
+let accountEmail = "dasmat@lpu.in"
+var accountPassword = "12345"
+accountCity = "Rourkela"
+let accountState
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+//accountId = 2 //not allowed
+
+accountEmail = "sunny.lpu.in"
+accountPassword = "87643"
+accountCity = "varanasi"
+
+console.log(accountId);
+console.log(accountEmail);
+
+//to print all the things in table we use console.table([])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+rkgnnvrgg lmg'
+sdfmg; '
+vbml;ncvl;dfbm '
+dfgvbml; v'functionalb ''
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+//"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we are using node js not browser
+
+console.log(3+3)
+console.log("Damsat")
+
+let name = "Dasmat"
+let age = 18
+let isLoggedIn = false
+let stale = null
+console.table([name,age,isLoggedIn,stale])
+
+// number => 2 to power 53
+//bigint
+//string => "",''
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined)  //to find the type
+console.log(typeof null)
