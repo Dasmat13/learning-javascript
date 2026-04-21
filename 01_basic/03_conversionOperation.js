3a14f9dcf394298a1b9c3302e0e5968031a4f493
Dasmat13 <mey37056@gmail.com>
2026-04-22 03:03:22 +0530

    datatypes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,25 +1,59 @@
-let score = "34avbd"
+// let score = "34avbd"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-//"33" => 33
-//"33abc" => NaN
-//true => 1; false => 0
+// //"33" => 33
+// //"33abc" => NaN
+// //true => 1; false => 0
 
-let isLoggedIn = "nm"
-let booleanIsLoggedIn  = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = "nm"
+// let booleanIsLoggedIn  = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 => true ; 0 => false
-// "" => false
-// "dasmat" => true 
+// // 1 => true ; 0 => false
+// // "" => false
+// // "dasmat" => true 
 
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber)
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber)
+
+//*********************** operations**********************
+
+// let value = 3
+// let negVlue = -value
+// console.log(negVlue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2^4);
+
+// let str1 = "hello "
+// let str2 = "dasmat"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log("1"+2+3);
+//  console.log(1+2+"3");
+//  console.log("1"+"2"+"3");
+
+ console.log(+true)
+console.log(+"")
+   
+let num1 , num2, num3
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter)
---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive
+/* 
+7 types : string, number, boolean, null, undefined, symbol, BigInt
+*/
+const score= 300
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemo = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol("123")
+
+console.log(id === anotherId)
+
+const bigNumber = 2356543234565n
+
+
+
+//refrence(non primitive)
+/*
+array, objects, function
+*/
+
+
+const heroes = ["spide",'hulk','thor']
+{
+    name1: "dasmat"
+    age: 22
+}
+let myObj = {
+    name1: "dasmat"
+    age: 22
+}
+
+
+const myFunction = function (){
+    console.log("hello Dasmat")
+}
+
+ 
