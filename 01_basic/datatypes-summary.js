f4faf367f32690233df33b75e39a43e30020d7ec
dasmat <164524276+Dasmat13@users.noreply.github.com>
2026-04-23 12:33:53 +0000

    stach and heap

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -1,42 +1,66 @@
-// primitive
-/* 
-7 types : string, number, boolean, null, undefined, symbol, BigInt
-*/
-const score= 300
-const scoreValue = 100.3
+// // primitive
+// /* 
+// 7 types : string, number, boolean, null, undefined, symbol, BigInt
+// */
+// const score= 300
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemo = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemo = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol("123")
+// const id = Symbol('123')
+// const anotherId = Symbol("123")
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
-const bigNumber = 2356543234565n
-
-
-
-//refrence(non primitive)
-/*
-array, objects, function
-*/
+// const bigNumber = 2356543234565n
 
 
-const heroes = ["spide",'hulk','thor']
-{
-    name1: "dasmat"
-    age: 22
+
+// //refrence(non primitive)
+// /*
+// array, objects, function
+// */
+
+
+// const heroes = ["spide",'hulk','thor']
+// {
+//     name1: "dasmat"
+//     age: 22
+// }
+// let myObj = {
+//     name1: "dasmat"
+//     age: 22
+// }
+
+
+// const myFunction = function (){
+//     console.log("hello Dasmat")
+// }
+
+//  ``
+
+//======================================================================
+
+//stack(primitive)idhar copy milta hai , heap (non-primitive) idhar refrence milta hai
+
+let myyoutubename = "dasmathansda"
+let anothername = myyoutubename
+
+
+anothername = "chaiwithcode"
+console.log(anothername)
+console.log(myyoutubename)
+
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
-let myObj = {
-    name1: "dasmat"
-    age: 22
-}
 
+let usertwo = userone
 
-const myFunction = function (){
-    console.log("hello Dasmat")
-}
+usertwo.email = "yoyyo@gmsail.com"
 
- 
+console.log(userone.email)
+console.log(usertwo.email)
