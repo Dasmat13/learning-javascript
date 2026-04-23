2d06c9543ac3159a9396a7962bbff35b4fe63afa
dasmat <164524276+Dasmat13@users.noreply.github.com>
2026-04-23 20:08:49 +0000

    nums and maths

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+// const score = 100
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(typeof(balance.toString()))
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 123.345643
+// console.log(otherNumber.toPrecision(6));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//++++++++++++++maths++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(5.9))
+// console.log(Math.sqrt(121))
+// console.log(Math.min(2,8,1,9,4,-2))
+// console.log(Math.max(6,46,35,356,3,67654))
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
