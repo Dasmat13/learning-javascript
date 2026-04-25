8cf56f4627df0509968e8e3faf798d1c8bf9a4e5
Dasmat13 <mey37056@gmail.com>
2026-04-25 23:07:16 +0530

    done

---
+++ 02_basic/05_object3.js
@@ -0,0 +1,18 @@
+const course = {
+    coursename: "creatine monohydrate",
+    price: "999",
+    courseInstructor: "dasmat"
+}
+
+console.log(course.courseInstructor); // dasmat
+const {courseInstructor: sir} =course
+//console.log(courseInstructor);//dasmat
+console.log(sir); //dasmat
+
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "dasmat")
+
