1b48fef0c88687a59a8d5ad838746c05c8b1e42d
Dasmat13 <mey37056@gmail.com>
2026-05-06 00:37:30 +0530

    docs: added 07_project folder

---
+++ 07_projects/01_project/script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+// buttons.forEach(function(button){
+//     button.addEventListener('click', function(e){
+//         console.log(e)
+//         console.log(e.target)
+//         if(e.target.id === 'grey'){
+//             body.style.backgroundColor = e.target.id;
+//         }
+//     });
+// });
+
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        body.style.backgroundColor = e.target.id
+    })
+})
